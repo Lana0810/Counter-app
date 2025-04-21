d37257ff97f5740362c569082bc230e8fd0dc18f
Lana <lananh.duong@gmail.com>
2025-04-21 21:22:09 +0930

    Initial commit

---
+++ src/Lesson 1/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function App() {
+  const [like, setLike] = useState(0); /*function trả về array có hai phần tử, giá trị khởi tạo của function useState là initialState cho là số 0*/
+  const handleClick = () => {
+    setLike(1 - like)
+  }
+  return (
+    <div>
+      <h1 onClick={handleClick}>{like === 1 ? "👍" : "👎"}</h1>
+      <h1 onClick={handleClick} style={{ color: like === 1 ? "red" : "black" }}>&#9829;</h1>
+    </div> 
+  );
+}
+
+export default App;
