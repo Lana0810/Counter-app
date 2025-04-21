6b62310241b4fe84f97f076423e14f8d6a02c102
Lana0810 <lananh.duong@gmail.com>
2025-04-21 21:29:23 +0930

    Delete src/Lesson 1 directory

--- src/Lesson 1/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // 👉 dùng "react-dom/client" thay vì "react-dom"
-import App from "./App";
-
-// 👉 dùng createRoot (cú pháp React 18)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
